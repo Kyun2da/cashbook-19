9f2960dd6232356d412c3361d0cfcee78e876c22
Kyun Heo <kyun2dot@gmail.com>
2021-07-27 23:03:35 +0900

    [feature/#11] state에 들어갈 데이터 정리 (#14)

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,4 +1,41 @@
-export interface IUser {
+interface RouterState {
+  pathname: string;
+  search: string;
+  hash: string;
+}
+
+interface LoginState {
+  avatar: string;
+  username: string;
+}
+
+interface Category {
+  id: number;
+  type: 'income' | 'expenditure';
+  name: string;
+  color: string;
+}
+
+interface Payment {
   id: number;
   name: string;
+}
+
+interface CashRecord {
+  id: number;
+  categoryId: number;
+  category: Category;
+  paymentId: number;
+  payment: Payment;
+  title: string;
+  value: number;
+  date: Date;
+}
+
+interface StoreState {
+  router: RouterState;
+  login: LoginState | null;
+  categories: Category[];
+  payments: Payment[];
+  records: CashRecord[];
 }
