9d8df53f1ae9a15bd1fbdb2af0a804d086ad7061
Taegyun Sooran Kim <T.SooranKim@gmail.com>
2021-07-27 20:47:15 +0900

    [feature/#10] router 추가, store와 연결, view rerender (#12)
    * feat: router 추가, store와 연결, view rerender
    
    * router.ts를 lib/로 이동

---
+++ client/src/views/header.ts
@@ -0,0 +1,46 @@
+import type { Observer } from '@/lib/observer';
+import type Router from '@/router';
+
+export default class Header implements Observer {
+  private router: Router;
+
+  constructor(router: Router) {
+    this.router = router;
+
+    const header = document.querySelector('header');
+    header.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  template(state: Record<string, any>): string {
+    return `
+      <div>HEADER ${state.router.pathname}</div>
+      <ul>
+        <li><a href="/a">/a로 이동</a></li>
+        <li><a href="/b">/b로 이동</a></li>
+        <li><a href="/c">/c로 이동</a></li>
+      </ul>
+    `;
+  }
+
+  render(state: Record<string, any>): void {
+    const markup = this.template(state);
+    const parent = document.querySelector('header');
+
+    parent.innerHTML = markup;
+  }
+
+  update(state: Record<string, any>): void {
+    this.render(state);
+  }
+
+  handleClick(e: Event): void {
+    const target = e.target as HTMLElement;
+
+    if (target instanceof HTMLAnchorElement) {
+      e.preventDefault();
+      this.router.push({
+        pathname: target.href,
+      });
+    }
+  }
+}
